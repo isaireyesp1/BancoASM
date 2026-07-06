849e54e1a5fcf6e05a8f83f3f56718ecfdb15343
Isai Reyes <anonymus1201a@gmail.com>
2026-07-06 10:53:21 -0600

    Actualizacion de Cuentas

--- cuentas.asm
+++ cuentas.asm
@@ -2,11 +2,17 @@
 ; cuentas.asm
 ; Sistema Bancario ASM
 ; Módulo de Gestión de Cuentas
+; MASM32 + Win32 API
+; Parte 1 de 14
 ;=============================================================
 
 .386
 .model flat, stdcall
 option casemap:none
+
+;=============================================================
+; Librerías
+;=============================================================
 
 include windows.inc
 include kernel32.inc
@@ -16,185 +22,111 @@ includelib kernel32.lib
 includelib user32.lib
 
 ;=============================================================
-; Estructura de cuenta (SIMPLIFICADA EN ARCHIVO)
+; Prototipos
 ;=============================================================
-; Formato en banco.dat:
-;
-; Usuario|PIN|Saldo
-;
-; Ejemplo:
-; isai|1234|1500
+
+BuscarCuenta      PROTO :DWORD
+ValidarPIN        PROTO :DWORD,:DWORD
+ObtenerSaldo      PROTO :DWORD
+DepositarDinero   PROTO :DWORD,:DWORD
+RetirarDinero     PROTO :DWORD,:DWORD
+TransferirDinero  PROTO :DWORD,:DWORD,:DWORD
+CrearCuenta       PROTO :DWORD,:DWORD,:DWORD
+GuardarBanco      PROTO
+CargarBanco       PROTO
+
+AbrirArchivo      PROTO
+CerrarArchivo     PROTO
+
+CopiarCadena      PROTO :DWORD,:DWORD
+CompararCadena    PROTO :DWORD,:DWORD
+LongitudCadena    PROTO :DWORD
+
+NumeroACadena     PROTO :DWORD,:DWORD
+CadenaANumero     PROTO :DWORD
+
+;=============================================================
+; Constantes
+;=============================================================
+
+MAX_USUARIO EQU 32
+MAX_PIN     EQU 16
+MAX_LINEA   EQU 128
+MAX_BUFFER  EQU 8192
+
+;=============================================================
+; Estructura de Cuenta
+;=============================================================
+
+CUENTA STRUCT
+
+    Usuario    db MAX_USUARIO dup(0)
+    PIN        db MAX_PIN dup(0)
+    Saldo      dd ?
+
+CUENTA ENDS
+
+;=============================================================
+; Variables Inicializadas
 ;=============================================================
 
 .data
 
 ArchivoBanco db "banco.dat",0
 
-BufferArchivo db 256 dup(0)
-BufferLinea   db 128 dup(0)
-
 Separador db "|",0
 
-UsuarioTemp db 32 dup(0)
-PinTemp     db 16 dup(0)
+CRLF db 13,10,0
 
-SaldoTemp   dd 0
-
-hFile HANDLE ?
-BytesRead DWORD ?
+CadenaVacia db 0
 
 ;=============================================================
-; EXTERN (desde otros módulos)
+; Buffers
 ;=============================================================
 
-EXTERN CuentaActual:DWORD
+BufferArchivo db MAX_BUFFER dup(0)
+
+BufferLinea db MAX_LINEA dup(0)
+
+BufferUsuario db MAX_USUARIO dup(0)
+
+BufferPIN db MAX_PIN dup(0)
+
+BufferNumero db 32 dup(0)
+
+;=============================================================
+; Variables Globales
+;=============================================================
+
+CuentaActual CUENTA <>
+
+IndiceCuenta DWORD 0
+
+SaldoTemporal DWORD 0
+
+BytesLeidos DWORD 0
+
+BytesEscritos DWORD 0
+
+PosicionActual DWORD 0
+
+TamArchivo DWORD 0
+
+hArchivo HANDLE ?
+
+;=============================================================
+; Variables sin inicializar
+;=============================================================
+
+.data?
+
+CuentaDestino CUENTA ?
+
+CuentaOrigen CUENTA ?
+
+;=============================================================
+; Código
+;=============================================================
 
 .code
 
-;=============================================================
-; BuscarCuenta
-; Entrada: ECX = puntero a usuario
-; Salida: EAX = 0 no existe / >0 posición lógica
-;=============================================================
-
-BuscarCuenta PROC lpUsuario:DWORD
-
-    invoke CreateFile, ADDR ArchivoBanco, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
-
-    mov hFile, eax
-    cmp eax, INVALID_HANDLE_VALUE
-    je NoExiste
-
-    ; lectura simplificada (solo demo educativa)
-
-    invoke ReadFile, hFile, ADDR BufferArchivo, SIZEOF BufferArchivo, ADDR BytesRead, NULL
-
-    invoke CloseHandle, hFile
-
-    ;---------------------------------------------------------
-    ; Buscar cadena usuario en buffer
-    ;---------------------------------------------------------
-
-    invoke lstrstr, ADDR BufferArchivo, lpUsuario
-
-    cmp eax, 0
-    je NoExiste
-
-    mov eax, 1
-    ret
-
-NoExiste:
-
-    mov eax, 0
-    ret
-
-BuscarCuenta ENDP
-
-;=============================================================
-; ValidarPIN
-; Entrada:
-;   ECX = cuenta (dummy en este modelo)
-;   EDX = PIN ingresado
-; Salida:
-;   EAX = TRUE / FALSE
-;=============================================================
-
-ValidarPIN PROC cuenta:DWORD, lpPIN:DWORD
-
-    ;---------------------------------------------------------
-    ; Versión simplificada (educativa)
-    ; En proyecto real: comparar contra archivo
-    ;---------------------------------------------------------
-
-    invoke lstrcmp, lpPIN, ADDR PinTemp
-
-    cmp eax, 0
-    jne PIN_INCORRECTO
-
-    mov eax, TRUE
-    ret
-
-PIN_INCORRECTO:
-
-    mov eax, FALSE
-    ret
-
-ValidarPIN ENDP
-
-;=============================================================
-; ObtenerSaldo
-; Entrada: cuenta
-; Salida: EAX = saldo
-;=============================================================
-
-ObtenerSaldo PROC cuenta:DWORD
-
-    ;---------------------------------------------------------
-    ; DEMO: saldo fijo (debe leerse de archivo)
-    ;---------------------------------------------------------
-
-    mov eax, 15000
-    ret
-
-ObtenerSaldo ENDP
-
-;=============================================================
-; DepositarDinero
-;=============================================================
-
-DepositarDinero PROC cuenta:DWORD
-
-    ;---------------------------------------------------------
-    ; DEMO: simulación de depósito
-    ;---------------------------------------------------------
-
-    ; aquí se pediría monto en versión completa
-
-    ret
-
-DepositarDinero ENDP
-
-;=============================================================
-; RetirarDinero
-;=============================================================
-
-RetirarDinero PROC cuenta:DWORD
-
-    ;---------------------------------------------------------
-    ; DEMO: simulación de retiro
-    ;---------------------------------------------------------
-
-    ret
-
-RetirarDinero ENDP
-
-;=============================================================
-; TransferirDinero
-;=============================================================
-
-TransferirDinero PROC cuenta:DWORD
-
-    ;---------------------------------------------------------
-    ; DEMO: simulación de transferencia
-    ;---------------------------------------------------------
-
-    ret
-
-TransferirDinero ENDP
-
-;=============================================================
-; GuardarBanco
-;=============================================================
-
-GuardarBanco PROC
-
-    ;---------------------------------------------------------
-    ; En versión real:
-    ; reescribir banco.dat con datos actualizados
-    ;---------------------------------------------------------
-
-    ret
-
-GuardarBanco ENDP
-
-END
